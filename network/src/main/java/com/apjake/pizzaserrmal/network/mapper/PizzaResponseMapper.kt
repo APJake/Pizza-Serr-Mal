b787f054552b76a95b2ae848821d4226e53d73a9
AP Jake <apjake@owl.local>
2022-04-14 14:51:24 +0630

    mapper done

--- network/src/main/java/com/apjake/pizzaserrmal/network/mapper/PizzaResponseMapper.kt
+++ network/src/main/java/com/apjake/pizzaserrmal/network/mapper/PizzaResponseMapper.kt
@@ -2,10 +2,12 @@ package com.apjake.pizzaserrmal.network.mapper
 
 import PizzaListResponse
 import com.apjake.pizzaserrmal.common.util.UniMapper
+import com.apjake.pizzaserrmal.domain.models.PizzaVO
 import com.apjake.pizzaserrmal.network.response.Pizza
 
-class PizzaResponseMapper: UniMapper<PizzaListResponse,List<Pizza> > {
-    override fun map(data: PizzaListResponse): List<Pizza> {
-        return data;
+class PizzaResponseMapper: UniMapper<PizzaListResponse,List<PizzaVO> > {
+    override fun map(data: PizzaListResponse): List<PizzaVO> {
+        return data.map { pizza -> PizzaVO(id = pizza.id.toString(), name = pizza.name, isVeg = pizza.veg,
+            description = pizza.description, image = pizza.img, price = pizza.img, isBookmark = false) }
     }
 }
